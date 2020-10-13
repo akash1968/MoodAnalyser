78348ff9d84caa64e07cc82298696271f37976d0
Akash <akash.singh670@gmail.com>
2020-10-13 21:45:23 +0530

    [Akash] .Added UC4 Reflection of Mood Analyser Assigment

--- MoodAnalyser Assignnment/MoodAnalyserException.cs
+++ MoodAnalyser Assignnment/MoodAnalyserException.cs
@@ -9,7 +9,7 @@ namespace MoodAnalyser
         public enum TypeOfException
         {
             NULL_MESSAGE, EMPTY_MESSAGE, NO_SUCH_FIELD, NO_SUCH_METHOD,
-            NO_SUCH_CLASS, OBJECT_CREATION_ISSUE
+            NO_SUCH_CLASS, OBJECT_CREATION_ISSUE, NO_SUCH_CONSTRUCTOR
         }
         public TypeOfException type;
         public MoodAnalyserCustomException(TypeOfException type, string message) : base(message)
---
+++ MoodAnalyser Assignnment/MoodAnalyserFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MoodAnalyser;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyserFactory
+    {
+        public static object CreateMoodAnalyse(string ClassName, string ConstructorName)
+        {
+            string pattern = @"." + ConstructorName + "$";
+            var result = Regex.Match(ClassName, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    Type moodAnalyserType = assembly.GetType(ClassName);
+                    return Activator.CreateInstance(moodAnalyserType);
+                }
+                catch (NullReferenceException)
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.TypeOfException.NO_SUCH_CLASS, "no such class is found");
+                }
+       }
+            else
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.TypeOfException.NO_SUCH_CONSTRUCTOR, "no such constructor found");
+        }
+
+    }
+}
