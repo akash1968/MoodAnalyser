be7673415b8a331b070c9fd36482a5911c0989f8
Akash <akash.singh670@gmail.com>
2020-10-15 01:33:13 +0530

    [Akash] .Added UC6-Invoke Method Using Reflection of Mood Analyser Assigment

--- MoodAnalyserMSTest/UnitTest1.cs
+++ MoodAnalyserMSTest/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace MoodAnalyserMSTest
     [TestClass]
     public class UnitTest1
     {
-        //GivenSadMoodShouldReturnSad
+        //UC 1.1: GivenSadMoodShouldReturnSad
         [TestMethod]
         [DataRow("I am in a sad mood")]
         public void TestMethod1(string message)
@@ -19,7 +19,7 @@ namespace MoodAnalyserMSTest
             //Assert
             Assert.AreEqual(expected, actual);
         }
-        //GivenAnyMoodShouldReturnHappy
+        //UC 1.2: GivenAnyMoodShouldReturnHappy
         [TestMethod]
         [DataRow("I am in any mood")]
         public void TestMethod2(string message)
@@ -32,7 +32,7 @@ namespace MoodAnalyserMSTest
             //Assert
             Assert.AreEqual(expected, actual);
         }
-        //Given_Empty_Mood_Should_Throw_MoodAnalysisCustomException_IndicatingEmptyMood
+        //UC 3.2: Given_Empty_Mood_Should_Throw_MoodAnalysisCustomException_IndicatingEmptyMood
         [TestMethod]
         public void TestMethod3()
         {
@@ -47,7 +47,7 @@ namespace MoodAnalyserMSTest
                 Assert.AreEqual("Mood Should not be empty", exception.Message);
             }
         }
-        //Given_Null_Mood_Should_Throw_MoodAnalysisCustomException_IndicatingNullMood
+        //UC 3.1: Given_Null_Mood_Should_Throw_MoodAnalysisCustomException_IndicatingNullMood
         [TestMethod]
         public void TestMethod4()
         {
@@ -70,7 +70,7 @@ namespace MoodAnalyserMSTest
            
                 string message = null;
             object expected = new MoodAnalyse(message);
-            object obj = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyser.MoodAnalyse", "MoodAnalyse");
+            object obj = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyser.MoodAnalyse", "MoodAnalyse",null);
             obj.Equals(expected);
 
         }
@@ -81,9 +81,38 @@ namespace MoodAnalyserMSTest
 
             string message = "HAPPY";
             object expected = new MoodAnalyse(message);
-            object obj = MoodAnalyserFactory.CreateMoodAnalyseWithParametrizedConstructor("MoodAnalyser.MoodAnalyse", "MoodAnalyse");
+            object obj = MoodAnalyserFactory.CreateMoodAnalyseWithParametrizedConstructor("MoodAnalyser.MoodAnalyse", "MoodAnalyse","HAPPY");
             obj.Equals(expected);
 
+        }
+        [TestMethod]
+        //UC 6.1 : Given 'Happy' message when proper should return 'Happy Mood'.
+        //GivenHappyMessage_InvokeAnalyseMoodMethod_ShouldReturnHappyMoodMessage
+        public void TestMethod7()
+        {
+            //arrange
+            string expected = "HAPPY Mood";
+            
+            //Act
+            string actual = MoodAnalyserFactory.InvokeAnalyserMethod("Happy","MoodAnalyse");
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        //GivenHappyMessage_WhenImproperMethod_ShouldThrowMoodAnalysisException
+        //UC 6.2 : Given Improper method name must return mood analyser custom exception
+        public void TestMethod8()
+        {
+            try
+            {
+                //Act
+                object result= MoodAnalyserFactory.InvokeAnalyserMethod("Happy", "MoodAnalyseDifferent");
+            }
+            catch (MoodAnalyserCustomException exception)
+            {
+                //Assert
+                Assert.AreEqual("Exception: method not found", exception.Message);
+            }
         }
     }
 }
